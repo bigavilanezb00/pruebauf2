e2cf30403a98086698dae348d9fbedf748af05ad
bryan <bigavilanezb00@elpuig.xeill.net>
2022-05-02 14:07:55 +0200

    Branch1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola món");
-        System.out.println("1");
+        System.out.println("Hola branch1");
     }
 }
