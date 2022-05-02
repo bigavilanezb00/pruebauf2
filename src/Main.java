cea2cf48e0b4c480831cd7281d1370b20d284b45
bryan <bigavilanezb00@elpuig.xeill.net>
2022-05-02 14:08:13 +0200

    branch2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola món");
-        System.out.println("2");
-        System.out.println("1");
+        System.out.println("Hola branch2");
     }
 }
